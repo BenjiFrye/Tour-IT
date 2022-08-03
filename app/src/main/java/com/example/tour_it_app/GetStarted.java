2c5d672e646a3b7dca055a950c7e310ac3d8f971
BenjiFrye <benjaminsfrye@gmail.com>
2022-08-03 20:22:58 +0200

    Initialisation of Tour-IT Application

--- app/src/main/java/com/example/tour_it_app/GetStarted.java
+++ app/src/main/java/com/example/tour_it_app/GetStarted.java
@@ -1,14 +1,51 @@
 package com.example.tour_it_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
+
+
+    // ---------------- * Component declaration * ------------------ //
+
+    private AppCompatButton btnGetStarted;
+    private Button btnLogin1;
+
+    // ------------------------------------------------------------ //
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        btnLogin1 = findViewById(R.id.btn_login_here);
+
+        // --------------------------------- * Listeners * -------------------------------------- //
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GetStarted.this, Register.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GetStarted.this, Login.class);
+                startActivity(i);
+            }
+        });
+
+        // -------------------------------------------------------------------------------------- //
     }
 }
