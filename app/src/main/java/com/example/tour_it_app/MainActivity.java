8389846cd303dab56dc11292dd5c9c1c2b229591
BenjiFrye <benjaminsfrye@gmail.com>
2022-09-18 17:31:45 +0200

    top menu bar

--- app/src/main/java/com/example/tour_it_app/MainActivity.java
+++ app/src/main/java/com/example/tour_it_app/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.tour_it_app;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout mainDrawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //---------------------------- Main Navigation Drawer layout -------------------------------
+        //Drawer layout instance to toggle menu icon to open
+        //and back button to close drawer
+        mainDrawer = findViewById(R.id.layout);
+
+        //------------------------------------------------------------------------------------------
+
+
     }
+
+    //----------------------------------- Drawer Management Code -----------------------------------
+    public void ClickMenu(View view)
+    {
+        //Open the drawer
+        openDrawer(mainDrawer);
+    }
+    public static void openDrawer(DrawerLayout drawerLayout)
+    {
+        //Open the drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public void ClickLogo(View view)
+    {
+        //Closes the drawer
+        closeDrawer(mainDrawer);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout)
+    {
+        //Close drawer layout
+        //Check condition
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            //When drawer is open
+            //Close Drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    //----------------------------------- Drawer Management Code -----------------------------------
 }
