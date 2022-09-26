7cc532542077c2d484df30d64679448d169d5b77
BenjiFrye <benjaminsfrye@gmail.com>
2022-09-26 18:30:00 +0200

    bottom bar

--- app/src/main/java/com/example/tour_it_app/MainActivity.java
+++ app/src/main/java/com/example/tour_it_app/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.tour_it_app;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ActionBarDrawerToggle drawerToggle;
+    private BottomNavigationView bottomView;
     private DrawerLayout mainDrawer;
     private Button btnSearch;
+
+    HomeFragment homeFrag = new HomeFragment();
+    SettingsFragment settingsFrag = new SettingsFragment();
+    FavouritesFragment favouritesFrag = new FavouritesFragment();
 
 
     @Override
@@ -30,11 +39,39 @@ public class MainActivity extends AppCompatActivity {
         mainDrawer = findViewById(R.id.layout);
         //------------------------------------------------------------------------------------------
 
-        //-------------------------------- Search Drawer layout ------------------------------------
+        //Finding Id's
         btnSearch = findViewById(R.id.btnMainSearch);
-        //------------------------------------------------------------------------------------------
+        bottomView = findViewById(R.id.bottomNavView);
+
+        //by default, load the home screen
+        bottomView.setSelectedItemId(R.id.bttm_home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,homeFrag).commit();
+
+        //Bottom Navigation Bar On Click
+        bottomView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(MenuItem item) {
+
+               switch (item.getItemId()){
+                   case R.id.bttm_home:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,homeFrag).commit();
+                       return true;
+                   case R.id.bttm_settings:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,settingsFrag).commit();
+                       return true;
+                   case R.id.bttm_favourites:
+                       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,favouritesFrag).commit();
+                       return true;
+               }
+
+               return false;
+           }
+       });
+
+
 
     }
+
 
     //----------------------------------- Drawer Management Code -----------------------------------
     public void ClickMenu(View view)
@@ -47,11 +84,7 @@ public class MainActivity extends AppCompatActivity {
         //Open the drawer layout
         drawerLayout.openDrawer(GravityCompat.START);
     }
-    public void ClickLogo(View view)
-    {
-        //Closes the drawer
-        closeDrawer(mainDrawer);
-    }
+
     public static void closeDrawer(DrawerLayout drawerLayout)
     {
         //Close drawer layout
@@ -66,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
     //----------------------------------- Drawer Management Code -----------------------------------
 
 
+    //------------------------------------ Search Dialogue -----------------------------------------
     public void ClickSearch(View view) {
         ShowSearchDialogue();
     }
@@ -73,4 +107,5 @@ public class MainActivity extends AppCompatActivity {
         SearchFragment dialog = new SearchFragment();
         dialog.show(getSupportFragmentManager(), "Search Dialogue");
     }
+    //------------------------------------ Search Dialogue -----------------------------------------
 }
