82f12ff4ec618a929d8dd1ad931d20cc60f7733b
BenjiFrye <benjaminsfrye@gmail.com>
2022-09-27 15:49:21 +0200

    updated navigation

--- app/src/main/java/com/example/tour_it_app/MainActivity.java
+++ app/src/main/java/com/example/tour_it_app/MainActivity.java
@@ -4,24 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity{
 
+    private NavigationView sideNavView;
     private BottomNavigationView bottomView;
     private DrawerLayout mainDrawer;
     private Button btnSearch;
+    private TextView txtHeading;
 
     HomeFragment homeFrag = new HomeFragment();
     SettingsFragment settingsFrag = new SettingsFragment();
@@ -42,25 +47,31 @@ public class MainActivity extends AppCompatActivity{
         //Finding Id's
         btnSearch = findViewById(R.id.btnMainSearch);
         bottomView = findViewById(R.id.bottomNavView);
+        sideNavView = findViewById(R.id.mainNavView);
+        txtHeading = findViewById(R.id.txtPageName);
 
         //by default, load the home screen
+        SetTopBarMain();
         bottomView.setSelectedItemId(R.id.bttm_home);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,homeFrag).commit();
 
-        //Bottom Navigation Bar On Click
-        bottomView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        //Bottom Navigation Bar menu item On Click
+        bottomView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
            @Override
            public boolean onNavigationItemSelected(MenuItem item) {
 
                switch (item.getItemId()){
                    case R.id.bttm_home:
                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,homeFrag).commit();
+                       SetTopBarMain();
                        return true;
                    case R.id.bttm_settings:
                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,settingsFrag).commit();
+                       SetTopBarOther("Settings");
                        return true;
                    case R.id.bttm_favourites:
                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,favouritesFrag).commit();
+                       SetTopBarOther("Favourites");
                        return true;
                }
 
@@ -68,10 +79,60 @@ public class MainActivity extends AppCompatActivity{
            }
        });
 
+        //Main Navigation Side Bar menu item on Click
+        sideNavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+                switch (item.getItemId()) {
+                    case R.id.main_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, homeFrag).commit();
+                        closeDrawer(mainDrawer);
+                        SetTopBarMain();
+                        bottomView.setSelectedItemId(R.id.bttm_home);
+                        return true;
+                    case R.id.main_search:
+                        ///
+                        closeDrawer(mainDrawer);
+                        return true;
+                    case R.id.main_fav:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, favouritesFrag).commit();
+                        closeDrawer(mainDrawer);
+                        SetTopBarOther("Favourites");
+                        bottomView.setSelectedItemId(R.id.bttm_favourites);
+                        return true;
+                    case R.id.main_acc:
+                        Intent i = new Intent(MainActivity.this, AccountActivity.class);
+                        startActivity(i);
+                        closeDrawer(mainDrawer);
+                        return true;
+                    case R.id.main_sett:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, settingsFrag).commit();
+                        closeDrawer(mainDrawer);
+                        SetTopBarOther("Settings");
+                        bottomView.setSelectedItemId(R.id.bttm_settings);
+                        return true;
+                    case R.id.main_logout:
+                        ///
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
 
+    private void SetTopBarMain() {
+        txtHeading.setText("LandmarkName");
+        txtHeading.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon_pin,0,0,0);
+        btnSearch.setVisibility(View.VISIBLE);
+    }
+
+    private void SetTopBarOther(String heading) {
+        txtHeading.setText(heading);
+        txtHeading.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+        btnSearch.setVisibility(View.INVISIBLE);
+    }
 
     //----------------------------------- Drawer Management Code -----------------------------------
     public void ClickMenu(View view)
