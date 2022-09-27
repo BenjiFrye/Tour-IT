badfdd6a43c1320cc625050e95a67d0df0a4d044
BenjiFrye <benjaminsfrye@gmail.com>
2022-09-27 16:38:03 +0200

    updated navigation 2

---
+++ app/src/main/java/com/example/tour_it_app/AccountFragment.java
@@ -0,0 +1,33 @@
+package com.example.tour_it_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AccountFragment extends Fragment {
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+
+    }
+}
--- app/src/main/java/com/example/tour_it_app/MainActivity.java
+++ app/src/main/java/com/example/tour_it_app/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.Gravity;
@@ -22,15 +23,18 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity{
 
+    //Variables
     private NavigationView sideNavView;
     private BottomNavigationView bottomView;
     private DrawerLayout mainDrawer;
     private Button btnSearch;
     private TextView txtHeading;
 
+    //New instances
     HomeFragment homeFrag = new HomeFragment();
     SettingsFragment settingsFrag = new SettingsFragment();
     FavouritesFragment favouritesFrag = new FavouritesFragment();
+    AccountFragment accFrag = new AccountFragment();
 
 
     @Override
@@ -92,7 +96,7 @@ public class MainActivity extends AppCompatActivity{
                         bottomView.setSelectedItemId(R.id.bttm_home);
                         return true;
                     case R.id.main_search:
-                        ///
+                        ShowSearchDialogue();
                         closeDrawer(mainDrawer);
                         return true;
                     case R.id.main_fav:
@@ -102,9 +106,10 @@ public class MainActivity extends AppCompatActivity{
                         bottomView.setSelectedItemId(R.id.bttm_favourites);
                         return true;
                     case R.id.main_acc:
-                        Intent i = new Intent(MainActivity.this, AccountActivity.class);
-                        startActivity(i);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, accFrag).commit();
                         closeDrawer(mainDrawer);
+                        SetTopBarOther("Account");
+                        bottomView.getMenu().findItem(R.id.bttm_invisible).setChecked(true);
                         return true;
                     case R.id.main_sett:
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, settingsFrag).commit();
@@ -120,14 +125,18 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+
     }
 
+
+    //Change appearance of top bar when home page is open
     private void SetTopBarMain() {
         txtHeading.setText("LandmarkName");
         txtHeading.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon_pin,0,0,0);
         btnSearch.setVisibility(View.VISIBLE);
     }
 
+    //Change appearance of top bar if any other page is open
     private void SetTopBarOther(String heading) {
         txtHeading.setText(heading);
         txtHeading.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
